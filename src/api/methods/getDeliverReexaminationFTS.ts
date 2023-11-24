43ab4b6633bbdfc1c66553beb262053208812519
Alexandr Kletnyy <q@obscur.space>
2023-11-24 12:28:30 +0300

    feat: integrate api conversion
    ref: #121422

--- src/api/methods/getDeliverReexaminationFTS.ts
+++ src/api/methods/getDeliverReexaminationFTS.ts
@@ -4,14 +4,10 @@ import type { DeliverReexaminationFTSItem } from '@/domain/types';
 interface RequestType extends RequestConfigType {}
 type ResponseType = Array<DeliverReexaminationFTSItem>;
 
-export default defaultHost.createRequest<RequestType, ResponseType>(
-  'get',
-  '/deliver-reexamination-fts',
-  {
-    isMock: import.meta.env.VITE_IS_MOCK ?? false,
+export default defaultHost.createRequest<RequestType, ResponseType>('get', '/block4', {
+  isMock: import.meta.env.VITE_IS_MOCK ?? false,
 
-    getMockData(): Promise<ResponseType> {
-      return import('../_mock/block-4.json').then(({ default: data }) => data);
-    },
+  getMockData(): Promise<ResponseType> {
+    return import('../_mock/block-4.json').then(({ default: data }) => data);
   },
-);
+});
