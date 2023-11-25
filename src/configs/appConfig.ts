d0975d3aae862ebfce2546bd8ede60c4b2b00967
Alexandr Kletnyy <q@obscur.space>
2023-11-25 18:51:08 +0300

    fix: api-key
    ref: #121351

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -2,5 +2,5 @@ export default {
   apiUrl: import.meta.env.VITE_API_URL,
   apiKey: import.meta.env.VITE_API_KEY,
 
-  isMock: Boolean(import.meta.env.VITE_IS_MOCK),
+  isMock: Boolean(+import.meta.env.VITE_IS_MOCK),
 };
