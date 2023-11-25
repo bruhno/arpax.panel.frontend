3970a6dc9582ead05d278455bf1e3dfc3f83f1a9
Данильченко Евгений <evgeniy.danilchenko@osinit.com>
2023-11-25 16:00:17 +0300

    feat: add InviteToInspectionArea

--- src/components/Scoreboard/InviteToInspectionArea/InviteToInspectionAreaTable.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="greetings">
-    Таблица - Приглашаем пройти в зону досмотра пассажиров
-  </div>
-</template>
-
-<style module lang="scss"></style>
